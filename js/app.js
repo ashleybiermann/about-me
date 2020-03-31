59ec046423e423e5453608f5543145e15a335984
Ashley <ashleybiermann@gmail.com>
2020-03-31 14:38:01 -0700

    outline of the script

---
+++ js/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+//greet the user and ask for their name
+//display their name back and welcome them to the site
+
+//prompt the user with FIVE questions to get to know me more
+//yes or no questions
+//take into consideration case sensitivity --HINT .toUpperCase() or .toLowerCase()
+
+//1. does ashley have a dog? - YES
+//console.log first, later change to alert with response
+
+//2. does ashley prefer sunny days over rainy days? - YES
+//console.log first, later change to alert with response
+
+//3. does ashley do yoga? - NO
+//console.log first, later change to alert with response
+
+//4. does ashley like to kayak? - YES
+//console.log first, later change to alert with response
+
+//5. does ashley like to bake? - NO
+//console.log first, later change to alert with response
+
+//display the user's name back to them as a final message

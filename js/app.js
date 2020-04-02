566142a45a0fe5309e7d6313514ed6642ad482b3
Ashley <ashleybiermann@gmail.com>
2020-04-01 20:37:51 -0700

    added a correct answers counter and reply to user

--- js/app.js
+++ js/app.js
@@ -7,9 +7,8 @@ var userName = prompt("Welcome! What is your name?");
 console.log("userName: " + userName);
 alert("Welcome, " + userName + "! It's great to see you. Take this mini quiz and review your answers to see how much you know about Ashley. Let's start!");
 
-//prompt the user with FIVE questions to get to know me more
-//yes or no questions
 //take into consideration case sensitivity --HINT .toUpperCase() or .toLowerCase()
+var correctAnswers = 0;
 
 //1. does ashley have a dog? - YES
 //console.log first
@@ -18,6 +17,8 @@ console.log(userName + "'s answer to 'has dog': " + haveADog);
 // alert with response telling them if correct
 if(haveADog === "YES") {
   alert("That's correct! Her name is Callie");
+  correctAnswers++;
+  console.log('correct dog answer! total correct answers: ' + correctAnswers);
 } else {
   alert("She actually does have a dog. Her name is Callie.")
 }
@@ -29,6 +30,8 @@ console.log(userName + "'s answer to 'sunny over rainy': " + sunnyOverRainy);
 //alert with response telling them if correct
 if(sunnyOverRainy === "YES") {
   alert("You've got it! She definitely prefers the sunshine.");
+  correctAnswers++;
+  console.log('correct sunny answer! total correct answers: ' + correctAnswers);
 } else {
   alert("Rain, rain, go away, Ashley prefers a sunny day. Let's try this one...");
 }
@@ -42,6 +45,8 @@ if(doesYoga === "YES") {
   alert("Nah, she never really got into it, and it still alludes her.");
 } else {
   alert("Correct! She does not really like yoga. There's just something about it...");
+  correctAnswers++;
+  console.log('correct yoga answer! total correct answers: ' + correctAnswers);
 }
 
 //4. does ashley like to kayak? - YES
@@ -51,6 +56,8 @@ console.log(userName + "'s answer to 'likes to kayak': " + likeToKayak);
 // alert with response telling them if correct
 if(likeToKayak === "YES") {
   alert("Indeed, and Callie comes with her, too!");
+  correctAnswers++;
+  console.log('correct kayak answer! total correct answers: ' + correctAnswers);
 } else {
   alert("Ashley DOES like to kayak. Callie comes with her, too!");
 }
@@ -64,6 +71,8 @@ if(likeToBake === "YES") {
   alert("She loves to eat baked goods, but she does NOT like to bake much on her own.");
 } else {
   alert("Exactly, she does not like to bake, but she does like to eat baked goods.");
+  correctAnswers++;
+  console.log('correct baking answer! total correct answers: ' + correctAnswers);
 }
 
 //Add another question, number guessing game
@@ -76,7 +85,9 @@ for(var i = 0; i < 4; i++) {
   var favNumGuess = prompt("What is Ashley's favorite number? Hint: It's between 1 and 10.");
   console.log(userName + "'s " + "number " + (i+1) + "guess: " + favNumGuess);
   if(favNumGuess == ashleysFavNum) {
-    (alert("Correct! Her favorite number IS 4!"))
+    alert("Correct! Her favorite number IS 4!")
+    correctAnswers++;
+    console.log('correct kayak answer! total correct answers: ' + correctAnswers);
     break; //breaking free of the loop once they got it right
     } else if(favNumGuess < ashleysFavNum) {
         alert("Too low, try a higher number.");
@@ -90,29 +101,26 @@ if(favNumGuess != 4) {
   alert("That's enough guessing for one day... her favorite number is 4.");
 }
 
-// add question - user guesses to a question with multiple answers
+//7. question - user guesses to a question with multiple answers
 
-// for(var i = 0; i < 6; i++) {}
-
-var ashleysStates = ['michigan', 'washington'];
+var ashleysStates = ['Michigan', 'Washington'];
 
   for(var i = 0; i < 6; i++) {
     var stateGuess = prompt('Ashley has lived in two states. Try to guess one!');
     console.log(userName + "'s state guess: " + stateGuess);
     if(stateGuess === ashleysStates[0] || stateGuess === ashleysStates[1]) {
       alert('Correct! She has lived in TWO states, though: ' + ashleysStates[0] + " and " + ashleysStates[1]);
+      correctAnswers++;
+      console.log('correct state answer! total correct answers: ' + correctAnswers);
       break;
     }
     else {
       alert('Cool state! But Ashley hasn\'t lived there. Try again.');
     }
   }  
-
-
-
-
-
-
+if(stateGuess !== ashleysStates[0] || stateGuess !== ashleysStates[1]) {
+  alert('The two states Ashley has lived in are: ' + ashleysStates[0] + " and " + ashleysStates[1]);
+}
   
 //display the user's name back to them as a final message
-alert("Thank you, " + userName + " for taking Ashley's 'Getting to Know Me' quiz. Check out this page to learn more!");
+alert('Thank you, ' + userName + 'for taking Ashley\'s \'Getting to Know Me\' quiz. You got ' + correctAnswers + ' answers correct out of 7. Check out this page to learn more!');
